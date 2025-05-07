385d67d6929fbb5711acb6cd5faab2fbab78fb28
capt-farvez <farvez.capt@gmail.com>
2025-05-08 04:46:35 +0600

    Add Celery configuration to settings.py

--- library_api/settings.py
+++ library_api/settings.py
@@ -134,3 +134,10 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
     }
 }
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_TIMEZONE = 'UTC'
